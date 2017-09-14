f564ea40bcfaf7ab25819ec2f364e01d77f51491
Dan Abend <DanAbend@rtonational.com>
2017-09-14 11:16:16 -0400

    parse excel and display json

---
+++ main.js
@@ -0,0 +1,23 @@
+/* set up XMLHttpRequest */
+var url = "2016ResidentBusinessLicenseList_201612201241106417.xlsx";
+var oReq = new XMLHttpRequest();
+oReq.open("GET", url, true);
+oReq.responseType = "arraybuffer";
+
+oReq.onload = function(e) {
+  var arraybuffer = oReq.response;
+
+  /* convert data to binary string */
+  var data = new Uint8Array(arraybuffer);
+  var arr = new Array();
+  for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+  var bstr = arr.join("");
+
+  /* Call XLSX */
+  var workbook = XLSX.read(bstr, {type:"binary"});
+
+  /* DO SOMETHING WITH workbook HERE */
+  document.write(XLSX.utils.sheet_to_json(workbook));
+}
+
+oReq.send();
